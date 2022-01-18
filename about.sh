c6dbd145493eb2798a0949c153e432fab687ab4b
zeaking <85456530+zeaking@users.noreply.github.com>
2022-01-18 11:02:22 +0700

    Create about.sh

---
+++ about.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#         Premium Auto Script By ZEAKING       #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By ZEAKING                        #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# ALLAH SWT                                    #"
+echo -e "# Google                                        #"
+echo -e "#                                               #"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
